2cd9be319fd7e7478bf224c907f8e76b2322ffc9
zackmason6 <82919928+zackmason6@users.noreply.github.com>
2024-08-20 10:28:33 -0400

    videoChunker2.3
    Removed deprecated code, added comments, added message when starting split video method. Added actual field names in metadata submission section.

--- videoChunker.py
+++ videoChunker.py
@@ -30,8 +30,7 @@ def video_operation():
         segment_length = chunkLengthEntry.get()
         threading.Thread(target=split_video, args=(videoFilePath, segment_length, update_progress)).start()
         #split_video(videoFilePath, segment_length)
-        #messagebox.showinfo("File Found", "Segment length read as: " + str(segment_length))
-
+        messagebox.showinfo("Video split operation started. Progress bar will update shortly...")
 
 # Function to handle the submit button click
 def submit_data():
@@ -45,9 +44,22 @@ def submit_data():
     field8_data = entry8.get()
     field9_data = entry9.get()
     field10_data = entry10.get()
+    field11_data = entry11.get()
+    field12_data = entry12.get()
+    field13_data = entry13.get()
+    field14_data = entry14.get()
+    field15_data = entry15.get()
+    field16_data = entry16.get()
+    field17_data = entry17.get()
+    field18_data = entry18.get()
+    field19_data = entry19.get()
+    field20_data = entry20.get()
+    field21_data = entry21.get()
+    field22_data = entry22.get()
+    field23_data = entry23.get()
     
     # File name for the CSV
-    filename = 'data.csv'
+    filename = 'metadata.csv'
     
     # Check if the file exists to determine whether to write headers
     file_exists = os.path.isfile(filename)
@@ -58,13 +70,13 @@ def submit_data():
         
         # Write headers if the file is being created for the first time
         if not file_exists:
-            writer.writerow(["Field1", "Field2", "Field3","Field4","Field5","Field6","Field7","Field8","Field9","Field10"])
+            writer.writerow(["Project ID (or CruiseID)", "DiveID", "Dive Site Name","Collecting Platform Name","Camera Type/Code","Dive Start Date","Dive End Date","Dive Start Time","Dive End Time","Dive Duration","Dive On Bottom Timestamp", "Dive Off Bottom Timestamp", "Max Depth (m)", "Minimum Depth (m)", "North Latitude", "South Latitude", "East Longitude", "West Longitude", "Project/Cruise Abstract","Dive Abstract", "Dive Objectives", "Dive Keywords", "Underwater Cultural Heritage (UCH) Restrictions?"])
         
         # Write the data from the text fields
         writer.writerow([field1_data, field2_data, field3_data, field4_data, field5_data, field6_data, field7_data, field8_data, field9_data, field10_data])
     
     # Inform the user that the data has been saved
-    messagebox.showinfo("Data Saved", "The data has been successfully saved to the 'data' spreadsheet in your working directory.")
+    messagebox.showinfo("Data Saved", "The data has been successfully saved to the 'metadata' spreadsheet in your working directory.")
     
     # Clear the entry fields after submission (optional)
     entry1.delete(0, tk.END)
@@ -77,39 +89,19 @@ def submit_data():
     entry8.delete(0, tk.END)
     entry9.delete(0, tk.END)
     entry10.delete(0, tk.END)
-
-def getFileList(myDirectory,extension):
-    """This function gets a list of files from the myDirectory variable.
-    Specifically, the os.listdir command gets a list of everything in the directory
-    and then this function iterates over that list and grabs everything where the
-    filename ends with csv. These are then added to another list.
-
-    Args:
-        myDirectory (string): This directory is specified by the user.
-
-    Returns:
-        list: This is a list of xml files in the user's current directory.
-    """
-    dataFiles = []
-    myFiles = os.listdir(myDirectory)
-    for file in myFiles:
-        if file.endswith(extension):
-            dataFiles.append(file)
-    return dataFiles
-
-#os.environ["IMAGEIO_FFMPEG_EXE"] = r"c:\users\zachary.t.mason\desktop\coding\codingprojects\ncrmp_dataanalysis\.venv\lib\site-packages\ffmpeg"
-def startMenu():
-    whitelist = [1,2,3]
-    print("Please enter the corresponding number of your choice from the menu below:")
-    print("1. Split video file into chunks by time")
-    print("2. Split video file into chunks by size")
-    print("3. Generate metadata")
-
-    userSelection = input("Your selection: ")
-    if int(userSelection) not in whitelist:
-        print("\nInvalid selection\n")
-        startMenu()
-    return(userSelection)
+    entry11.delete(0, tk.END)
+    entry12.delete(0, tk.END)
+    entry13.delete(0, tk.END)
+    entry14.delete(0, tk.END)
+    entry15.delete(0, tk.END)
+    entry16.delete(0, tk.END)
+    entry17.delete(0, tk.END)
+    entry18.delete(0, tk.END)
+    entry19.delete(0, tk.END)
+    entry20.delete(0, tk.END)
+    entry21.delete(0, tk.END)
+    entry22.delete(0, tk.END)
+    entry23.delete(0, tk.END)
 
 def video_upload():
     filename = videoFileNameEntry.get()
@@ -127,55 +119,6 @@ def video_upload():
     video_information_box.insert('4.0', "\nSize per minute of video: " + str(sizePerMinute) + " GB")
 
     video_information_box.insert('5.0', "\n\nIn order to receive video chunks smaller than 5GB, specify that your chunks are less than " + str(idealChunkSize) + " seconds long.")
-
-def split_video2(filename, segment_length): #DEPRECATED
-    segment_length = int(segment_length)
-    clip = VideoFileClip(filename)
-    duration = clip.duration
-    print("Segment length read as: " + str(segment_length))
-    print("Filename read as: " + str(filename))
-    print("Duration read as: " + str(duration))
-
-    start_time = 0
-    end_time = segment_length
-    i = 1
-    basename = os.path.basename(filename).split('.')[0]
-    extension = os.path.basename(filename).split('.')[1]
-
-    while start_time < duration:
-        output = os.path.join(f"{basename}_part{i}."+str(extension))
-        print("Output listed as: " + str(output))
-        
-        # Construct ffmpeg command
-        #command = [
-        #    ffmpeg_path,
-        #    '-ss', str(start_time),
-        #    '-i', filename,
-        #    '-to', str(min(end_time, duration)),
-        #    '-c:v', 'libx264',  # Set video codec to H.264
-        #    '-c:a', 'aac',      # Set audio codec to AAC
-        #    '-strict', 'experimental',  # Allow experimental codecs if needed
-        #    output
-        #]
-
-        # Construct ffmpeg command
-        command = [
-            ffmpeg_path,
-            '-ss', str(start_time),
-            '-i', filename,
-            '-to', str(min(end_time, duration)),
-            '-c', 'copy',
-            output
-        ]
-
-        # Run the command
-        subprocess.run(command, check=True)
-
-        start_time = end_time
-        end_time += segment_length
-        i += 1
-
-    print(f'Video split into {i-1} parts.')
 
 def split_video(filename, segment_length, progress_callback):
     segment_length = int(segment_length)
@@ -196,6 +139,18 @@ def split_video(filename, segment_length, progress_callback):
         output = os.path.join(f"{basename}_part{i}."+str(extension))
         print("Output listed as: " + str(output))
         
+        # Construct ffmpeg command
+        #command = [
+        #    ffmpeg_path,
+        #    '-ss', str(start_time),
+        #    '-i', filename,
+        #    '-to', str(min(end_time, duration)),
+        #    '-c:v', 'libx264',  # Set video codec to H.264
+        #    '-c:a', 'aac',      # Set audio codec to AAC
+        #    '-strict', 'experimental',  # Allow experimental codecs if needed
+        #    output
+        #]
+
         # Construct ffmpeg command
         command = [
             ffmpeg_path,
@@ -243,41 +198,6 @@ def getData(dataSheet,columnHeader):
   cellValue = dataSheet.loc[1, columnHeader]
   return cellValue
 
-def readMetadataIn():
-    myDirectory = os.getcwd()
-    listOfLocalFiles = getFileList(myDirectory,".xlsx")
-    print("Here are your local files: ")
-    for item in listOfLocalFiles:
-        print(str(item))
-
-    metadataFile = getMetadataFile(listOfLocalFiles)
-    xlsx = pd.ExcelFile(metadataFile)
-    df1 = pd.read_excel(xlsx, sheet_name='Dive Specific Metadata')
-    #print(df1)
-
-    myDict = {}
-
-    myColumns = df1.columns
-    for column in myColumns:
-        tempValue = getData(df1, column)
-        tempDict = {column:tempValue}
-        myDict.update(tempDict)
-        #print(str(column) + ":" + str(tempValue))
-
-    print("Here is the full dictionary: ")
-    for key in myDict.keys():
-        print(str(key) + ": " + str(myDict.get(key)))
-
-    wordDocList = getFileList(myDirectory,".docx")
-    print("Here are your local files: ")
-    for item in wordDocList:
-        print(str(item))
-    wordDocFile = getMetadataFile(wordDocList)
-    word = docx.Document(wordDocFile)
-    print(str(len(word.paragraphs)))
-    for paragraph in word.paragraphs:
-        print(str(paragraph.text))
-
 def create_page_content(page):
     # Top frame
     top_frame = tk.Frame(page)
@@ -304,12 +224,10 @@ def create_page_content(page):
     separator.pack(fill=tk.X, padx=10, pady=10)
 
 if __name__ == "__main__":
-    userChoice = startMenu()
-    print("You selected: " + str(userChoice))
 
     # Initialize the main application window
     app = tk.Tk()
-    app.title("Video Chunker")
+    app.title("OER Video Chunker")
 
     # Center the window on the screen
     app.update_idletasks()  # Ensure correct dimensions are calculated
@@ -341,7 +259,7 @@ if __name__ == "__main__":
     page2 = ttk.Frame(notebook)
     notebook.add(page2, text="Split Video")
 
-    # Second page (tab)
+    # Third page (tab)
     page3 = ttk.Frame(notebook)
     notebook.add(page3, text="Generate Metadata")
 
@@ -422,10 +340,6 @@ if __name__ == "__main__":
     split_video_button = tk.Button(page2, text="Split Video", command=video_operation, bg="lightGrey", fg="black")
     split_video_button.pack(pady=20)
 
-    # Bold separator (using a Frame)
-    #separator_frame = tk.Frame(page2, height=3, bg="black")
-    #separator_frame.pack(fill=tk.X, padx=10, pady=10)
-
     ############################### PAGE 3 STARTS HERE #############################
 
     # Create a frame that will contain the canvas and the scrollbar
@@ -470,77 +384,128 @@ if __name__ == "__main__":
     # Bind the mousewheel event to the canvas
     canvas.bind_all("<MouseWheel>", _on_mousewheel)
 
-    label1 = tk.Label(label_frame, text="Field 1:", justify=tk.LEFT, anchor="w")
+    label1 = tk.Label(label_frame, text="Project ID (or CruiseID):", justify=tk.LEFT, anchor="w")
     label1.pack(pady=5, fill=tk.X, expand=True)
     entry1 = tk.Entry(label_frame)
     entry1.pack(pady=5, fill=tk.X, expand=True)
 
-    label2 = tk.Label(label_frame, text="Field 2:", justify=tk.LEFT, anchor="w")
+    label2 = tk.Label(label_frame, text="DiveID:", justify=tk.LEFT, anchor="w")
     label2.pack(pady=5, fill=tk.X, expand=True)
     entry2 = tk.Entry(label_frame)
     entry2.pack(pady=5, fill=tk.X, expand=True)
 
-    label3 = tk.Label(label_frame, text="Field 3:", justify=tk.LEFT, anchor="w")
+    label3 = tk.Label(label_frame, text="Dive Site Name:", justify=tk.LEFT, anchor="w")
     label3.pack(pady=5, fill=tk.X, expand=True)
     entry3 = tk.Entry(label_frame)
     entry3.pack(pady=5, fill=tk.X, expand=True)
 
-    label4 = tk.Label(label_frame, text="Field 4:", justify=tk.LEFT, anchor="w")
+    label4 = tk.Label(label_frame, text="Collecting Platform Name:", justify=tk.LEFT, anchor="w")
     label4.pack(pady=5, fill=tk.X, expand=True)
     entry4 = tk.Entry(label_frame)
     entry4.pack(pady=5, fill=tk.X, expand=True)
 
-    label5 = tk.Label(label_frame, text="Field 5:", justify=tk.LEFT, anchor="w")
+    label5 = tk.Label(label_frame, text="Camera Type/Code:", justify=tk.LEFT, anchor="w")
     label5.pack(pady=5, fill=tk.X, expand=True)
     entry5 = tk.Entry(label_frame)
     entry5.pack(pady=5, fill=tk.X, expand=True)
 
-    label6 = tk.Label(label_frame, text="Field 6:", justify=tk.LEFT, anchor="w")
+    label6 = tk.Label(label_frame, text="Dive Start Date:", justify=tk.LEFT, anchor="w")
     label6.pack(pady=5, fill=tk.X, expand=True)
     entry6 = tk.Entry(label_frame)
     entry6.pack(pady=5, fill=tk.X, expand=True)
 
-    label7 = tk.Label(label_frame, text="Field 7:", justify=tk.LEFT, anchor="w")
+    label7 = tk.Label(label_frame, text="Dive End Date:", justify=tk.LEFT, anchor="w")
     label7.pack(pady=5, fill=tk.X, expand=True)
     entry7 = tk.Entry(label_frame)
     entry7.pack(pady=5, fill=tk.X, expand=True)
 
-    label8 = tk.Label(label_frame, text="Field 8:", justify=tk.LEFT, anchor="w")
+    label8 = tk.Label(label_frame, text="Dive Start Time:", justify=tk.LEFT, anchor="w")
     label8.pack(pady=5, fill=tk.X, expand=True)
     entry8 = tk.Entry(label_frame)
     entry8.pack(pady=5, fill=tk.X, expand=True)
 
-    label9 = tk.Label(label_frame, text="Field 9:", justify=tk.LEFT, anchor="w")
+    label9 = tk.Label(label_frame, text="Dive End Time:", justify=tk.LEFT, anchor="w")
     label9.pack(pady=5, fill=tk.X, expand=True)
     entry9 = tk.Entry(label_frame)
     entry9.pack(pady=5, fill=tk.X, expand=True)
 
-    label10 = tk.Label(label_frame, text="Field 10:", justify=tk.LEFT, anchor="w")
+    label10 = tk.Label(label_frame, text="Dive Duration:", justify=tk.LEFT, anchor="w")
     label10.pack(pady=5, fill=tk.X, expand=True)
     entry10 = tk.Entry(label_frame)
     entry10.pack(pady=5, fill=tk.X, expand=True)
-    # Add more widgets to the label_frame if needed
+
+    label11 = tk.Label(label_frame, text="Dive on Bottom Timestamp:", justify=tk.LEFT, anchor="w")
+    label11.pack(pady=5, fill=tk.X, expand=True)
+    entry11 = tk.Entry(label_frame)
+    entry11.pack(pady=5, fill=tk.X, expand=True)
+
+    label12 = tk.Label(label_frame, text="Dive Off Bottom Timestamp:", justify=tk.LEFT, anchor="w")
+    label12.pack(pady=5, fill=tk.X, expand=True)
+    entry12 = tk.Entry(label_frame)
+    entry12.pack(pady=5, fill=tk.X, expand=True)
+
+    label13 = tk.Label(label_frame, text="Max Depth (m):", justify=tk.LEFT, anchor="w")
+    label13.pack(pady=5, fill=tk.X, expand=True)
+    entry13 = tk.Entry(label_frame)
+    entry13.pack(pady=5, fill=tk.X, expand=True)
+
+    label14 = tk.Label(label_frame, text="Minimum Depth (m):", justify=tk.LEFT, anchor="w")
+    label14.pack(pady=5, fill=tk.X, expand=True)
+    entry14 = tk.Entry(label_frame)
+    entry14.pack(pady=5, fill=tk.X, expand=True)
+
+    label15 = tk.Label(label_frame, text="North Latitude:", justify=tk.LEFT, anchor="w")
+    label15.pack(pady=5, fill=tk.X, expand=True)
+    entry15 = tk.Entry(label_frame)
+    entry15.pack(pady=5, fill=tk.X, expand=True)
+
+    label16 = tk.Label(label_frame, text="South Latitude:", justify=tk.LEFT, anchor="w")
+    label16.pack(pady=5, fill=tk.X, expand=True)
+    entry16 = tk.Entry(label_frame)
+    entry16.pack(pady=5, fill=tk.X, expand=True)
+
+    label17 = tk.Label(label_frame, text="East Longitude:", justify=tk.LEFT, anchor="w")
+    label17.pack(pady=5, fill=tk.X, expand=True)
+    entry17 = tk.Entry(label_frame)
+    entry17.pack(pady=5, fill=tk.X, expand=True)
+
+    label18 = tk.Label(label_frame, text="West Longitude:", justify=tk.LEFT, anchor="w")
+    label18.pack(pady=5, fill=tk.X, expand=True)
+    entry18 = tk.Entry(label_frame)
+    entry18.pack(pady=5, fill=tk.X, expand=True)
+
+    label19 = tk.Label(label_frame, text="Project/Cruise Abstract:", justify=tk.LEFT, anchor="w")
+    label19.pack(pady=5, fill=tk.X, expand=True)
+    entry19 = tk.Entry(label_frame)
+    entry19.pack(pady=5, fill=tk.X, expand=True)
+
+    label20 = tk.Label(label_frame, text="Dive Abstract:", justify=tk.LEFT, anchor="w")
+    label20.pack(pady=5, fill=tk.X, expand=True)
+    entry20 = tk.Entry(label_frame)
+    entry20.pack(pady=5, fill=tk.X, expand=True)
+    
+    label21 = tk.Label(label_frame, text="Dive Objectives:", justify=tk.LEFT, anchor="w")
+    label21.pack(pady=5, fill=tk.X, expand=True)
+    entry21 = tk.Entry(label_frame)
+    entry21.pack(pady=5, fill=tk.X, expand=True)
+
+    label22 = tk.Label(label_frame, text="Dive Keywords:", justify=tk.LEFT, anchor="w")
+    label22.pack(pady=5, fill=tk.X, expand=True)
+    entry22 = tk.Entry(label_frame)
+    entry22.pack(pady=5, fill=tk.X, expand=True)
+
+    label23 = tk.Label(label_frame, text="Underwater Cultural Heritage (UCH) Restrictions?", justify=tk.LEFT, anchor="w")
+    label23.pack(pady=5, fill=tk.X, expand=True)
+    entry23 = tk.Entry(label_frame)
+    entry23.pack(pady=5, fill=tk.X, expand=True)
 
     # Update the scrollregion after adding all the widgets
     label_frame.update_idletasks()
     canvas.config(scrollregion=canvas.bbox("all"))
 
-    # Create a submit button
+    # Create submit button and add it to page3
     submit_button = tk.Button(page3, text="Generate Metadata", command=submit_data, bg="lightGrey", fg="black")
     submit_button.pack(pady=20)
 
-    # Run the application
+    # Generate GUI
     app.mainloop()
-
-    #cruiseID = getData(df1,'Project ID (or CruiseID)')
-    #diveID = getData(df1,'DiveID')
-    #print("HERE IS WHAT YOU EXTRACTED FROM THE FILE: " + str(testString))
-
-    #cruise = input("Enter the cruise name: ")
-    #diveSite = input("Enter the dive site name: ")
-
-    #video_path = r'C:\Users\Zachary.T.Mason\Desktop\PS2220_DIVE0982_20220504_MOHAWKROVHD.mov'
-    #segment_length = int(segment_length)
-    #segment_length = 60
-    #output_dir = sys.argv[3]  # third argument from command line
-    #split_video(video_path, segment_length)
